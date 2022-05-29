ede027842438a1f3307618b2a8e7f12afdccc154
jp durigan <jpdurigan@gmail.com>
2022-05-29 13:07:41 -0300

    Fix Checkpoint

--- src/levels/_base/Checkpoint.gd
+++ src/levels/_base/Checkpoint.gd
@@ -12,6 +12,9 @@ extends LevelSpawner
 
 #--- private variables - order: export > normal var > onready -------------------------------------
 
+var _is_active : bool = false
+var _was_activated : bool = false
+
 onready var _animated_sprite : AnimatedSprite = $AnimatedSprite
 
 ### -----------------------------------------------------------------------------------------------
@@ -21,6 +24,7 @@ onready var _animated_sprite : AnimatedSprite = $AnimatedSprite
 
 func _ready():
 	_animated_sprite.play("off")
+	Events.connect("checkpoint_reached", self, "_on_checkpoint_reached")
 
 ### -----------------------------------------------------------------------------------------------
 
@@ -28,15 +32,31 @@ func _ready():
 ### Public Methods --------------------------------------------------------------------------------
 
 func activate() -> void:
+	if _is_active or _was_activated:
+		return
 	Events.emit_signal("checkpoint_reached", self)
 	_animated_sprite.play("activate")
+	_is_active = true
+	_was_activated = true
+
+
+func deactivate() -> void:
+	_animated_sprite.play("off")
+	_is_active = false
 
 ### -----------------------------------------------------------------------------------------------
 
 
 ### Private Methods -------------------------------------------------------------------------------
 
+func _on_checkpoint_reached(checkpoint) -> void:
+	if checkpoint != self and _is_active:
+		deactivate()
+
+
 func _on_Trigger_player_triggered():
+	if _was_activated:
+		return
 	activate()
 
 
