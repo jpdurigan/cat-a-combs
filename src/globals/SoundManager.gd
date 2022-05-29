26bd8389a079e6dcd1b63fe8d5be6a8ddf8db35f
jp durigan <jpdurigan@gmail.com>
2022-05-29 13:34:07 -0300

    Cria tela inicial maneira

--- src/globals/SoundManager.gd
+++ src/globals/SoundManager.gd
@@ -10,6 +10,7 @@ extends Node
 
 var BGM_BUS = AudioServer.get_bus_index("BGM")
 var SFX_BUS = AudioServer.get_bus_index("SFX")
+var WORLD_BUS = AudioServer.get_bus_index("World")
 
 #--- public variables - order: export > normal var > onready --------------------------------------
 
@@ -31,7 +32,7 @@ onready var _tween: Tween = $Tween
 
 ### Built in Engine Methods -----------------------------------------------------------------------
 
-func _ready():
+func _enter_tree():
 	Events.connect("main_menu_entered", self, "_on_main_menu_entered")
 	Events.connect("level_started", self, "_on_level_started")
 
@@ -74,10 +75,14 @@ func _set_is_sfx_on(value: bool) -> void:
 
 
 func _on_main_menu_entered() -> void:
+	AudioServer.set_bus_mute(WORLD_BUS, true)
+	if not is_instance_valid(_bgm_player):
+		yield(self, "ready")
 	play_bgm(menu_music)
 
 
 func _on_level_started() -> void:
+	AudioServer.set_bus_mute(WORLD_BUS, false)
 	play_bgm(game_music)
 
 ### -----------------------------------------------------------------------------------------------
